c2fbea4f696c55320da1049cd2fd068ed406974b
Emmanuel Chacón <emmanuelechra@gmail.com>
2021-10-19 14:19:36 -0400

    E-Commerce

--- src/App.js
+++ src/App.js
@@ -1,25 +1,12 @@
-import logo from './logo.svg';
-import './App.css';
-
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+import Header  from  "./componentes/NavBar"
+const App = () => {
+    return(
+        <>
+            <Header/>
+            <footer>
+                <p>Emmanuel Chacón &copy;</p>
+            </footer>
+        </>
+    )
 }
-
-export default App;
+export default App
---
+++ src/componentes/NavBar.js
@@ -0,0 +1,13 @@
+const  Header = () => {
+    return(
+        <header>
+            <h1>E-commerce</h1>
+            <nav>
+                <a href="#">Inicio</a>
+                <a href="#">Contacto</a>  
+            </nav>
+        </header>
+    )
+}
+
+export default Header

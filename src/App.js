b57b4e06f28ee4cf38b6b790b04ef03523906ffd
Emmanuel Chacón <emmanuelechra@gmail.com>
2021-11-09 20:02:53 -0400

    protecto1

--- src/App.js
+++ src/App.js
@@ -2,7 +2,7 @@ import { BrowserRouter, Route, Switch } from "react-router-dom"
 import Header from "./Header"
 import ItemListContainer from "./ItemListContainer"
 import ItemDetailContainer from "./ItemDetailContainer"
-import Cart from "./Cart"
+
 import Footer from "./Footer"
 import { CustomProvider } from "./cartContext"
 
@@ -16,7 +16,7 @@ const App = () => {
                     <Route path="/" component={ ItemListContainer } exact />
                     <Route path="/cat/:cat" component={ ItemListContainer } />
                     <Route path="/item/producto:id" component={ ItemDetailContainer } />
-                    <Route path="/cart" component={ Cart } />
+                    
                 </Switch>
                 <Footer />
             </CustomProvider>
